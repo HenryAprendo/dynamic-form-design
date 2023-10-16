fd06e4d13c3b1643ebc78712d638e0334a263a10
HenryAprendo <hjsmipuc@gmail.com>
2023-10-16 12:33:38 -0500

    Servicio para crear grupos de controles de formularios segun la información de cada instancia

---
+++ src/app/services/question-control.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { QuestionBase } from '../base-class/question-base';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuestionControlService {
+
+  /**
+   * Metodo para crear controles de formularios e incluirlos en grupo de formulario, según la información de cada instancia.
+   *
+   * @param questions Instancias del tipo QuestionBase(TextBoxQuestion,DropdownQuestion)
+   * @returns Un grupo de formulario
+   */
+  toFormGroup(questions:QuestionBase<string>[]): FormGroup<any> {
+    const group: any = {};
+
+    questions.forEach(question => {
+      group[question.key] = question.required ? new FormControl(question.value ?? '', Validators.required)
+                                              : new FormControl(question.value ?? '');
+    });
+
+    return new FormGroup(group);
+  }
+
+}
